23f9f8f2ac3a378d757f6f8ea45d0f78084b6b6f
ali raza sulehria <alirazasulehria@gmail.com>
2019-10-31 14:54:00 +0100

    Push Pop Shift Unshift Difference

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var Array=["23","54","75"];
+Array.push("5","12","14","72","9");
+
+console.log(Array);
+
+/*------ */
+
+
+
+var Array=["23","54","75"];
+Array.unshift("15","112","114","172","999");
+
+console.log(Array);
+
+/*-----*/
+
+var Array=["15","112","114","172","999"];
+Array.pop();
+Array.pop();
+
+console.log(Array);
+
+/*--------*/
+
+
+var Array=["15","112","114","172","999"];
+Array.shift();
+Array.shift();
+
+console.log(Array);
+
